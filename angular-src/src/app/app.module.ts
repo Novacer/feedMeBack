19af98209d5ac3f79a2f0b2cc74ee5f6b0e90d20
Wei Min Loh <32558880+wmloh@users.noreply.github.com>
2018-03-04 02:28:44 -0500

    changes to navbar

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ConfigComponent } from './components/config/config.component';
 import { ViewComponent } from './components/view/view.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const appRoutes : Routes = [
   {path: "", component: HomeComponent},
@@ -31,7 +32,8 @@ const appRoutes : Routes = [
     RegisterComponent,
     LoginComponent,
     ConfigComponent,
-    ViewComponent
+    ViewComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
