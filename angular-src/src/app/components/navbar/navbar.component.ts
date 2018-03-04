e063c8f5292a985a62f2e207e6f495228b2a1574
Jackamac-attack <zhangjack27@gmail.com>
2018-03-04 03:54:15 -0500

    prepare to deploy to heroku

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  constructor(private authService: AuthService) {
+  constructor(public authService: AuthService) {
  }
 
   ngOnInit() {
