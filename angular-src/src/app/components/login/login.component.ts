cbf03e97352697a0bcfba304f59fdd86f0488177
Jackamac-attack <zhangjack27@gmail.com>
2018-03-03 21:25:51 -0500

    implemented login.component.ts

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  user: User;
+  loginFail: boolean;
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.email = "";
+    this.password = "";
+    this.loginFail = false;
+ }
 
   ngOnInit() {
   }
 
+  onLoginSubmit() {
+    if (this.email == "" || this.password == "" || this.email == undefined || this.password == undefined) {
+      this.loginFail = true;
+    }
+
+    else {
+      let user = {email: this.email, password: this.password};
+
+      this.authService.loginUser(user).subscribe(data => {
+        if (data.success) {
+          let response_user = data.user;
+          this.loginFail = false;
+          localStorage.setItem("user", JSON.stringify(response_user));
+
+          this.router.navigateByUrl('/');
+        }
+        else {
+          this.loginFail = true;
+        }
+      });
+    }
+  }
+}
+
+interface User {
+  email: string;
+  password: string;
 }
