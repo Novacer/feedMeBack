88898d795fd7d94e2589be306e0481395136163a
Jackamac-attack <zhangjack27@gmail.com>
2018-03-03 20:25:21 -0500

    implemented basic auth service functions

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class AuthService {
+  user: any;
 
-  constructor() { }
+  constructor(private http: Http) {
 
+  }
+
+  registerUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post("https://localhost:8080/users/register", user, {headers: headers})
+      .map(resp => resp.json());
+  }
+
+  loginUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post("https://localhost:8080/users/login", user, {headers: headers})
+      .map(resp => resp.json());
+  }
+
+  updateFeedback(email, feedback) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let body = {email: email, feedback: feedback};
+
+    return this.http.post("https://localhost:8080/users/feedback", body, {headers: headers})
+      .map(resp => resp.json());
+
+  }
 }
